d936d5c02e584c5d4605a62094ffbace94b5a4ac
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2022-11-08 22:43:18 +0300

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Utku Kaan Gülsoy on 8.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
